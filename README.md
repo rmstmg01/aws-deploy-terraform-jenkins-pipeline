1f0b720881bdb46005f06148caa9cdb0b8002445
Ramesh Tamang <11027110+rmstmg01@users.noreply.github.com>
2021-10-04 12:41:45 +0545

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# aws-deploy-terraform-jenkins-pipeline
+This project is about Deploying Resources on AWS using Terraform and then deploying a Jenkins CI/CD pipeline.
---
+++ backend.tf
@@ -0,0 +1,9 @@
+# --- root/backend.tf ---
+
+terraform {
+  backend "s3" {
+    bucket = "week-24-project-092021065"
+    key    = "remote.tfstate"
+    region = "us-east-1"
+  }
+}
